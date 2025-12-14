9d7706e665972735611004fb268f10e58c7598de
BunnyBae <07112006kz@mail.ru>
2025-12-14 03:07:27 +0300

    complete brain-calc game

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Shiniluvv/frontend-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Shiniluvv/frontend-project-44/actions)
 [![asciicast](https://asciinema.org/a/BVuGNrA9lpkoppPYnd96NLbvB.svg)](https://asciinema.org/a/BVuGNrA9lpkoppPYnd96NLbvB)
+[![brain-calc demo](https://asciinema.org/a/e7LGTdoo08mvQTBq7uAwoDyEW.svg)](https://asciinema.org/a/e7LGTdoo08mvQTBq7uAwoDyEW)
---
+++ src/games/calc.js
@@ -0,0 +1,27 @@
+import getRandomNumber from '../utils.js';
+
+const operators = ['+', '-', '*'];
+
+const calculate = (num1, num2, operator) => {
+  switch (operator) {
+    case '+': return num1 + num2;
+    case '-': return num1 - num2;
+    case '*': return num1 * num2;
+    default: throw new Error(`Unknown operator: ${operator}`);
+  }
+};
+
+const generateRound = () => {
+  const num1 = getRandomNumber(1, 100);
+  const num2 = getRandomNumber(1, 100);
+  const operator = operators[getRandomNumber(0, operators.length - 1)];
+  
+  const question = `${num1} ${operator} ${num2}`;
+  const answer = String(calculate(num1, num2, operator));
+  
+  return { question, answer };
+};
+
+const getGameRules = () => 'What is the result of the expression?';
+
+export { generateRound, getGameRules };
